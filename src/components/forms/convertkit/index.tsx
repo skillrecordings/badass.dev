fcd3d4e627b178e64f9962fdd6ec152b416c33c1
Joel Hooks <joelhooks@gmail.com>
2021-04-01 10:05:16 -0700

    fix build for real

--- src/components/forms/convertkit/index.tsx
+++ src/components/forms/convertkit/index.tsx
@@ -8,7 +8,7 @@ type ConvertkitSubscribeFormProps = {
 
 const ConvertkitSubscribeForm: React.FC<ConvertkitSubscribeFormProps> = ({
   children,
-  onSubmit,
+  onSubmit = () => {},
 }) => {
   return (
     <div id="subscribe">
